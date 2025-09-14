92934e9a84139c5d43ee72f76cb30cbbb9dcc6fd
Adrijan <adrijangithub@gmail.com>
2025-09-14 22:09:53 +0200

    Create global.d.ts

---
+++ packages/nextjs/types/global.d.ts
@@ -0,0 +1,25 @@
+interface Window {
+  ethereum?: {
+    request: (args: { method: string; params?: any[] }) => Promise<any>;
+    on: (event: string, callback: (...args: any[]) => void) => void;
+    removeListener: (event: string, callback: (...args: any[]) => void) => void;
+    selectedAddress?: string;
+    chainId?: string;
+  };
+}
+
+interface EthereumProvider {
+  request: (args: { method: string; params?: any[] }) => Promise<any>;
+  on: (event: string, callback: (...args: any[]) => void) => void;
+  removeListener: (event: string, callback: (...args: any[]) => void) => void;
+  selectedAddress?: string;
+  chainId?: string;
+}
+
+declare global {
+  interface Window {
+    ethereum?: EthereumProvider;
+  }
+}
+
+export {};
